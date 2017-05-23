6ffafb5d219588b0d95fadbb0b73fd6ba6d20ea6
marskr <marskr@opoczta.pl>
2017-05-23 17:18:33 +0200

    Pente

---
+++ pente/build/CMakeFiles/pente.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/marcin/Dokumenty/PSZT/pente/src/main.cpp" "/home/marcin/Dokumenty/PSZT/pente/build/CMakeFiles/pente.dir/src/main.cpp.o"
+  "/home/marcin/Dokumenty/PSZT/pente/src/pente.cpp" "/home/marcin/Dokumenty/PSZT/pente/build/CMakeFiles/pente.dir/src/pente.cpp.o"
+  "/home/marcin/Dokumenty/PSZT/pente/src/player.cpp" "/home/marcin/Dokumenty/PSZT/pente/build/CMakeFiles/pente.dir/src/player.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ pente/build/CMakeFiles/pente.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pente.dir/src/main.cpp.o"
+  "CMakeFiles/pente.dir/src/pente.cpp.o"
+  "CMakeFiles/pente.dir/src/player.cpp.o"
+  "pente.pdb"
+  "pente"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pente.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
