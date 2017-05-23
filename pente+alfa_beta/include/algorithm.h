e21ebd05bfad2c558ea70e4ac2481d5062cc869e
marskr <marskr@opoczta.pl>
2017-05-24 00:28:54 +0200

    Add files via upload

---
+++ pente+alfa_beta/include/algorithm.h
@@ -0,0 +1,27 @@
+#ifndef ALGORITHM_H
+#define ALGORITHM_H	
+
+const int N = 19;
+
+struct Move
+{
+	int row, col;
+};
+
+//BASIC CLASS FOR "EVALUATION"
+class Algorithm
+{
+public:
+	Algorithm(){}
+	~Algorithm(){}
+	Move findBestMove(char board[N][N]);
+	char player = 'b', opponent = 'w', empty = 'o';
+private:
+	bool isMovesLeft(char board[N][N]);
+	//int evaluate(char b[N][N]);
+	int minimax(char board[N][N], int depth, bool isMax);
+	int abminimax(char board[N][N], int depth, bool isMax);	
+	int alfabeta(char board[N][N], int depth, bool isMax, int alfa, int beta);
+};
+
+#endif // ALGORITHM
