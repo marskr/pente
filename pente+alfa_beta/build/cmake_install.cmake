c775128e57e093a48f24a2e8ecd39aab479629ac
marskr <marskr@opoczta.pl>
2017-05-30 15:05:41 +0200

    Add files via upload

--- pente+alfa_beta/build/cmake_install.cmake
+++ pente+alfa_beta/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/marcin/Dokumenty/PSZT/pente+alfa_beta/pente-master/pente+alfa_beta
+# Install script for directory: /home/marcin/Dokumenty/PSZT/pente+alfa_beta
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/marcin/Dokumenty/PSZT/pente+alfa_beta/pente-master/pente+alfa_beta/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/marcin/Dokumenty/PSZT/pente+alfa_beta/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
